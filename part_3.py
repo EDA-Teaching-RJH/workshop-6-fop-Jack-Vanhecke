eca0a6b393a96fc980d171e918adf8cd534ec117
Jack-Vanhecke <jv11@kent.ac.uk>
2026-02-11 01:00:01 +0000

    Part 3 code

--- part_3.py
+++ part_3.py
@@ -1,1 +1,25 @@
+def get_coordinate():
+    while True:
+        try:
+            x_coord = int(input("What is the x-coordinate? "))
 
+            if -100 <= x_coord <= 100:
+                return(x_coord)
+                break
+            else:
+                print("Coordinate out of range.")
+                
+        except ValueError:
+            print("Invalid coordinate.")
+
+def main():
+
+    try:
+        speed = int(input("Enter motor speed. "))
+        print("Speed set to: " + str(speed))
+    except ValueError:
+        print("Error: Corrupted signal. Maintaining current speed.")
+    
+    print(str(get_coordinate()))
+
+main()
