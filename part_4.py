2ca031a6d90065095990e37c111e25db438caa2e
Jack-Vanhecke <jv11@kent.ac.uk>
2026-02-11 01:17:11 +0000

    Part 4 code

--- part_4.py
+++ part_4.py
@@ -1,1 +1,25 @@
+def main():
 
+    travel_log = []
+
+    while True:
+        try:
+            sensor = int(input("Sensor reading (slope angle): "))
+
+            travel_log.append(sensor)
+            
+            if sensor > 45:
+                print("CRITICAL TILT! HALTING.")
+                break
+
+        except ValueError:
+
+            print("Sensor Glitch.")
+
+        print("Path Stable. Moving forward.")
+
+    print("Mission Terminated.")
+    print("Total steps taken: " + str(len((travel_log))))
+    print(travel_log)
+    
+main()
